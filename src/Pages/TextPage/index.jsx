662140ae3229b91d05be7ea54cb919b5cbd1f530
CaptainBeatty <juliengrange.dev@gmail.com>
2024-01-01 22:13:10 +0100

    some design

--- src/Pages/TextPage/index.jsx
+++ src/Pages/TextPage/index.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import JSON from "../../JSON/Pictures.json";
 import { useParams } from "react-router-dom";
-import "../../Styles/GalleryPage.css"
+import "../../Styles/TextPage.css"
 
 
 function TextPage() {
   const { id } = useParams()
   const picture = JSON.find(item => item.id === id).url
+  const title = JSON.find(item => item.id === id).title
+  const description = JSON.find(item => item.id === id).description
     
     return (
         <div className="Picture">
             <div className="PictureTxt">
-                <h1>Text from pictures</h1>
+                <h1>{title}</h1>
                 <img src={picture} />
-                <p>C'était une belle balade. Il y avait le soleil...et Ksenia.</p>
+                <p>{description}</p>
             </div>
         </div>
     )
