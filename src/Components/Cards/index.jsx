d8b33ec7671519f88c2b21bfa3c3a97f7a109f89
CaptainBeatty <juliengrange.dev@gmail.com>
2024-02-05 22:49:58 +0100

    try a new photo

--- src/Components/Cards/index.jsx
+++ src/Components/Cards/index.jsx
@@ -1,18 +1,47 @@
 import { Link } from "react-router-dom";
 import JSON from "../../JSON/Pictures.json";
 import "../../Styles/Card.css";
+import { useState, useEffect } from 'react'
 
-function Cards () {
+
+function Cards() {
+
+    const [cardsList, setCards] = useState([])
+    const [isDataLoading, setDataLoading] = useState(false)
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+        async function fetchCards() {
+            setDataLoading(true)
+            try {
+                const response = await fetch(`http://localhost:8000/freelances`)
+                const { cardsList } = await response.json()
+                setCards(cardsList)
+            }
+            catch(err) {
+                console.log('===== error =====', err)
+                setError(true)
+            }
+            finally {
+                setDataLoading(false)
+            }
+        }
+        fetchCards()
+    }, [])
+
+    if (error) {
+        return <span>Oups, il y eu un problème</span>
+    }
+
     return (
         <div className="cards">
-            {JSON.map ((element) => (
-                <Link to = {`/textpage/${element.id}`} 
-                    key = {element.id} 
-                    className="card"
-                >
-                    <img src = { element.url } alt = { element.title } />
-                    <p> { element.title }</p>
-                </Link>
+            {cardsList.map ((profile, index) => (
+                <div>
+                key={`${profile.name}-${index}`}
+                label={profile.job}
+                picture={profile.picture}
+                title={profile.name}
+                </div>
             ))}
         </div> 
     )
